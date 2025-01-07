7b0baf2c45f5f8e30cbb16e4b59044f406e27243
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2025-01-07 21:45:28 +0100

    Add more tokenizers (#15)
    * Add tokenizers
    
    * Fix bug where the tokenizer wasn't used in the confusion matrix

--- python/stringalign/tokenize.py
+++ python/stringalign/tokenize.py
@@ -1,5 +1,6 @@
 import re
 import unicodedata
+from collections.abc import Iterable
 from typing import Literal, Protocol
 
 import stringalign._stringutils
@@ -7,6 +8,8 @@ import stringalign._stringutils
 
 class Tokenizer(Protocol):
     def __call__(self, text: str) -> list[str]: ...
+
+    def join(self, text: Iterable[str]) -> str: ...
 
 
 class StringNormalizer:
@@ -88,3 +91,82 @@ class GraphemeClusterTokenizer:
         clusters = stringalign._stringutils.grapheme_clusters(text)
         clusters = [self.post_clustering_normalizer(cluster) for cluster in clusters]
         return clusters
+
+    def join(self, tokens: Iterable[str]) -> str:
+        return "".join(tokens)
+
+
+class UnicodeWordTokenizer:
+    """Turn a text string into a list of extracted words as described in :cite:p:`unicode-annex-29`.
+
+    This code uses the ```unicode_segmentation```_ Rust crate to do split the text string into
+    words. Note that all punctuation is removed.
+
+    Arguments
+    ---------
+    normalization:
+        Which unicode normalization to use
+    case_insensitive:
+        If true, run `str.casefold` to make all letters lowercase
+    normalize_whitespace:
+        Turn any occurance of one or more whitespaces into exactly one regular space
+    remove_non_word_characters:
+        Remove any character that isn't a Unicode word character as well as underscores.
+
+    .. _``unicode_segmentation``: https://docs.rs/unicode-segmentation/latest/unicode_segmentation/index.html
+    """
+
+    def __init__(
+        self,
+        pre_clustering_normalizer: StringNormalizer | None = None,
+        post_clustering_normalizer: StringNormalizer | None = None,
+    ) -> None:
+        self.pre_clustering_normalizer = pre_clustering_normalizer or StringNormalizer()
+        self.post_clustering_normalizer = post_clustering_normalizer or StringNormalizer()
+
+    def __call__(self, text: str) -> list[str]:
+        text = self.pre_clustering_normalizer(text)
+        clusters = stringalign._stringutils.unicode_words(text)
+        clusters = [self.post_clustering_normalizer(cluster) for cluster in clusters]
+        return clusters
+
+    def join(self, tokens: Iterable[str]) -> str:
+        return " ".join(tokens)
+
+
+class SplitAtWordBoundaryTokenizer:
+    """Turn a text string into a list of words by splitting at word boundaries as described in :cite:p:`unicode-annex-29`.
+
+    This code uses the ```unicode_segmentation```_ Rust crate to do split the text string into
+    words.
+
+    Arguments
+    ---------
+    normalization:
+        Which unicode normalization to use
+    case_insensitive:
+        If true, run `str.casefold` to make all letters lowercase
+    normalize_whitespace:
+        Turn any occurance of one or more whitespaces into exactly one regular space
+    remove_non_word_characters:
+        Remove any character that isn't a Unicode word character as well as underscores.
+
+    .. _``unicode_segmentation``: https://docs.rs/unicode-segmentation/latest/unicode_segmentation/index.html
+    """
+
+    def __init__(
+        self,
+        pre_clustering_normalizer: StringNormalizer | None = None,
+        post_clustering_normalizer: StringNormalizer | None = None,
+    ) -> None:
+        self.pre_clustering_normalizer = pre_clustering_normalizer or StringNormalizer()
+        self.post_clustering_normalizer = post_clustering_normalizer or StringNormalizer()
+
+    def __call__(self, text: str) -> list[str]:
+        text = self.pre_clustering_normalizer(text)
+        clusters = stringalign._stringutils.split_at_word_boundaries(text)
+        clusters = [self.post_clustering_normalizer(cluster) for cluster in clusters]
+        return clusters
+
+    def join(self, tokens: Iterable[str]) -> str:
+        return " ".join(tokens)
