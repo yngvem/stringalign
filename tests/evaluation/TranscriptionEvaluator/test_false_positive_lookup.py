c6f633113a2cc768faabfccabb2a55abf0ac82fe
Marie Roald <roald.marie@gmail.com>
2025-07-11 07:04:57 +0200

    Add indices between line errors and false positives and negatives (#58)

---
+++ tests/evaluation/TranscriptionEvaluator/test_false_positive_lookup.py
@@ -0,0 +1,22 @@
+import pytest
+from stringalign.evaluation import TranscriptionEvaluator
+
+
+def test_simple_example() -> None:
+    evaluator = TranscriptionEvaluator.from_strings(
+        references=["abc", "def", "aaa"],
+        predictions=["bbc", "deg", "abb"],
+    )
+
+    assert evaluator.false_positive_lookup["b"] == frozenset({evaluator.line_errors[0], evaluator.line_errors[2]})
+    assert evaluator.false_positive_lookup["g"] == frozenset({evaluator.line_errors[1]})
+
+
+def test_key_error_for_non_existent_false_positive() -> None:
+    evaluator = TranscriptionEvaluator.from_strings(
+        references=["abc", "def", "aaa"],
+        predictions=["bbc", "deg", "abb"],
+    )
+
+    with pytest.raises(KeyError):
+        _ = evaluator.false_positive_lookup["a"]
