c7fc6e0fc51a4b351c415400716c7d74f93bb4e4
Marie Roald <roald.marie@gmail.com>
2026-02-19 06:58:48 +0000

    Run pre-commit
    Co-authored-by: Yngve Mardal Moe <yngve.m.moe@gmail.com>

--- examples/scripts/plot_tokenizations.py
+++ examples/scripts/plot_tokenizations.py
@@ -2,13 +2,14 @@
 Overview of tokenizations
 =====================
 
-This example shows the different tokenization methods available in Stringalign 
+This example shows the different tokenization methods available in Stringalign
 and how you can easily add your own custom tokenization if you have any specific needs.
 
 For these examples we will not add any string normalization (see :ref:`this example for more on that <example_normalization>`).
 See :ref:`here <cer_example>` for an example that adds a string normalization to a tokenizer.
 And the :mod:`stringalign.normalize` API documentation for an overview of the normalization options available in Stringalign.
 """
+
 import stringalign
 
 example_sentence = "Hello World! This is fun (example) sentence no. 10 000.😶‍🌫️"
@@ -37,7 +38,7 @@ print(tokenizer(example_sentence))
 tokenizer = stringalign.tokenize.UnicodeWordTokenizer()
 print(tokenizer(example_sentence))
 
-# %% 
+# %%
 # Custom tokenizer using `nb_tokenizer`
 # -------------------------------------
 import nb_tokenizer
