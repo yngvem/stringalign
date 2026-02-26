cc0541a7e229249f4d039b3af4027b00f832b306
Marie Roald <roald.marie@gmail.com>
2026-02-26 22:07:20 +0100

    Bugfix for confusables and some updates to config after last release (#82)
    Co-authored-by: Yngve Mardal Moe <yngve.m.moe@gmail.com>

--- tests/normalize/test_resolve_confusables.py
+++ tests/normalize/test_resolve_confusables.py
@@ -1,7 +1,7 @@
 from stringalign.normalize import resolve_confusables
 
 
-def test_simple_example():
+def test_simple_example() -> None:
     """Confusable characters are replaced with their intended counterparts for a known example"""
     confusables = {"𝓦": "W", "t": "𝓽"}
     example_text = "Hello, 𝓦orld! Here is a 𝓽est."
@@ -9,5 +9,13 @@ def test_simple_example():
     assert resolve_confusables(example_text, confusables) == expected_output
 
 
-# Note: The tests for the StringNormalizer class test various aspects of whitespace normalization also
+def test_multi_token_input() -> None:
+    """Multi-codepoint confusables are resolved correctly"""
+    confusables = {"ø": "oe", "aa": "å"}
+    example_text = "Brunost paa grovbrød"
+    expected_output = "Brunost på grovbroed"
+    assert resolve_confusables(example_text, confusables) == expected_output
+
+
+# Note: The tests for the StringNormalizer class test various aspects of confusable resolving also
 # but we keep this file as a simple example of how the resolve_confusables-function works.
